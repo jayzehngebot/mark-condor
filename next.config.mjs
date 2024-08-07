7e223ea04a3d647bfb60cc6cc32318483a30e2f3
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-08-07 10:42:56 -0400

    added bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
+    
