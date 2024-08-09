af442a6139dec52ea5f717557521dd4885f233f7
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-08-09 09:58:29 -0400

    h1 text styling across pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,8 @@
 
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="flex flex-col items-center justify-center h-auto p-10">
+            <h1 className="text-4xl font-bold mt-4">About MC</h1>
         </div>
     )
 }
--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Hire() {
     return (
-        <div>
-            <h1>Hire</h1>
+        <div className="flex flex-col items-center justify-center h-auto p-10">
+            <h1 className="text-4xl font-bold mt-4">MC For Hire</h1>
         </div>
     )
 }
