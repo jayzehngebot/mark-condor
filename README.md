1590a9fdd6559b2c2ae96a5c42f7121152a16989
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-08-15 14:34:10 -0400

    fix links and make sources entire card a link

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 Built with Next.js
 
+Github-Actions setup to deploy to ec2 instance behind ELB.
+AWS all setup for https://markcondor.com
+
 Curious to see what I can do in as short of a time as possible, given life and other obligations.
 
 Mark Condor is an Alt Persona : Think Marie Kondo but a dude who likes NASA, efficiency, and multimodal spaces.
 
 Also, this is a space for me to experiment with new ideas and technologies.
+
+Check out my 100%s?
+https://pagespeed.web.dev/analysis/https-www-markcondor-com/a2hsxsurj6?form_factor=mobile
 
 https://one.newrelic.com/nr1-core/infra-summary/host/NDYwMzI0N3xJTkZSQXxOQXwtODk4NDk5Nzc0OTA0MTU2NzE?account=4603247&duration=1800000&state=1cca7695-bebf-9cc9-ee5b-f839f2771e42
 
--- app/components/Basecard.tsx
+++ app/components/Basecard.tsx
@@ -8,9 +8,11 @@ export default function Basecard({ id, title, description, source_url }: { id: n
         // and fit three to a row
         <div id={`source_${id}`} className="bg-slate-900 rounded-lg shadow-md w-1/4 h-40 m-4">
             <a href={`${source_url}`} target="_blank" aria-label={`Visit ${title}`}>
-                <div className="p-4">
-                    <h2 className="text-xl mb-2">{title}</h2>
-                    <p className="text-gray-500">{description}</p>
+                <div className="flex flex-col h-full">
+                    <div className="p-4">
+                        <h2 className="text-xl mb-2">{title}</h2>
+                        <p className="text-gray-500">{description}</p>
+                    </div>
                 </div>
             </a>
         </div>
