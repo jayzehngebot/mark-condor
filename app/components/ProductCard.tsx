637ddfaf22b3efee40a26fb0f9f7969fd056ecad
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-08-22 10:35:28 -0400

    saving with nicer styles, esp on mobile, for the featured products cards

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -10,18 +10,22 @@ interface ProductData {
 
 export default function ProductCard({productData}: {productData: ProductData}) {
     return (
-        <li className="flex flex-col items-center">
-            <a href={productData.productURL} target="_blank">
-                <h1 className="text-slate text-center">{productData.name}</h1>
-                <Image
-                src={`/products/images/${productData.imageURL}`}
-                alt={productData.alt}
-                width={100}
-                height={100}
-                blurDataURL="data:..."
-                placeholder="blur"
-                style={{ width: 'auto', height: 'auto' }}
-                />
+        <li className="flex flex-col items-center h-32 sm:h-36 w-16 relative sm:h-auto sm:w-auto">
+            <a href={productData.productURL} target="_blank" className="flex flex-col h-full w-full sm:block">
+                <h1 className="text-slate text-center text-sm leading-tight" style={{ wordBreak: 'break-word', hyphens: 'auto', maxHeight: '3em' }}>{productData.name}</h1>
+                <div className="flex-grow relative w-full sm:static">
+                    <div className="absolute inset-x-0 bottom-0 sm:static">
+                        <Image
+                            src={`/products/images/${productData.imageURL}`}
+                            alt={productData.alt}
+                            width={100}
+                            height={100}
+                            blurDataURL="data:..."
+                            placeholder="blur"
+                            style={{ width: 'auto', height: 'auto' }}
+                        />
+                    </div>
+                </div>
             </a>
         </li>
     );
