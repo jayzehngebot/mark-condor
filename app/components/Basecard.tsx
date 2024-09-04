9c94f161189551d3a6d307ed87d83a3ea9453ced
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-09-04 13:38:38 -0400

    update images to webp, minor card style adjustment

--- app/components/Basecard.tsx
+++ app/components/Basecard.tsx
@@ -14,7 +14,7 @@ type BasecardProps = {
 
 const Basecard: React.FC<BasecardProps> = ({ id, title, description, source_url, wide, tags, image, openInNewTab, subhed }) => {
     return (
-        <div id={`source_${id}`} className={`bg-slate-900 rounded-lg shadow-md ${wide ? 'w-full' : 'w-full sm:w-1/2 md:w-1/4 lg:w-1/4'} h-32 sm:h-40 m-2 mb-8 mt-0`} style={{ backgroundImage: `url('/backgrounds/${id}.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div id={`source_${id}`} className={`bg-slate-900 rounded-lg shadow-md ${wide ? 'w-full' : 'w-full sm:w-1/2 md:w-1/4 lg:w-1/4'} h-32 sm:h-40 m-2 mb-4 mt-0`} style={{ backgroundImage: `url('/backgrounds/${id}.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
             <a href={`${source_url}`} target={openInNewTab ? "_blank" : "_self"} aria-label={`Visit ${title}`}>
                 <div className="flex flex-col h-full text-left">
                     <div className="p-4 h-full bg-opacity-90 bg-slate-900">
