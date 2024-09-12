883389854f70b6b4721737154a34f42a63f8a132
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-09-12 09:09:13 -0400

    saving with update on the project page

--- app/projects/[name]/page.tsx
+++ app/projects/[name]/page.tsx
@@ -4,8 +4,12 @@ import Image from 'next/image'
 
 async function getProjects() {
   try {
-      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-      const res = await fetch(`${baseUrl}/api/getProjects`);
+      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+      const res = await fetch(`${baseUrl}/api/getProjects`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
       if (!res.ok) {
           throw new Error(`Failed to fetch projects: ${res.statusText}`);
       }
@@ -37,7 +41,6 @@ interface Project {
 
 export default async function Page({ params }: { params: { name: string } }) {
     const projects: Project[] = await getProjects();
-
     const project = projects.find((p: Project) => p.url === params.name);
 
     if (!project) {
@@ -54,7 +57,6 @@ export default async function Page({ params }: { params: { name: string } }) {
             <h1 className="text-4xl text-center mt-4 text-slate-400 w-full pb-10">Project : {project.name}</h1>
             <h2 className="text-2xl text-center mt-4 text-slate-400 w-full pb-10">Primary Goal: {project.primary_goal}</h2>
             <Image priority={true} src={`/projectimages/${project.image}`} alt={project.name} width={600} height={600} />
-            {/* <h3 className="text-2xl text-center mt-4 text-slate-400 w-full pb-10">Secondary Goals: {project.secondary_goals}</h3> */}
         </div>
     )
   }
