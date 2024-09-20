fdea1def80e3db14a180bc5f3f88b7d7fb2e0b9d
Jay Zehngebot <jay.zehngebot@gmail.com>
2024-09-20 13:07:45 -0400

    quick fix spacing around newscard

--- app/components/SmallCard.tsx
+++ app/components/SmallCard.tsx
@@ -12,7 +12,7 @@ export default function SmallCard({ title, description, image_url, section, url 
     return (
             // Start of Selection
             <a href={`/${section}/${url}`}>
-                <div className="bg-slate-900 rounded-lg min-h-[198px] w-full sm:max-w-[212px] mt-4 mb-4 p-2 shadow-md">
+                <div className="bg-slate-900 rounded-lg min-h-[198px] sm:max-w-[212px] mr-4 mt-4 mb-4 p-2 shadow-md">
                     <Image src={`${image_url}`} alt={title} width={100} height={50} className="w-full h-20 object-cover rounded-md mb-2" />
                     <h3 className="text-lg text-gray-800">{title}</h3>
                     <p className="text-sm text-gray-600">{description}</p>
